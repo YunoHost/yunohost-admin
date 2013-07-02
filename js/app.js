f7832aefe8ec56b73301802dcaed4277d6cfa37e
Kload <kload@kload.fr>
2013-07-02 12:35:31 +0200

    Prettify & document

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 app = Sammy('#main', function (sam) {
 
+    /**
+     * Sammy Configuration
+     *
+     */
+    // Plugins
     sam.use('Mustache', 'ms');
 
+    // Initialize storage
     var store = new Sammy.Store({name: 'storage', type: 'session'});
 
+
+    /**
+     * Helpers
+     *
+     */
     sam.helpers({
+
+        // Flash helper to diplay instant notifications
         flash: function (level, message) {
             flashs = store.get('flash');
             if (!flashs) { flashs = {'info': [], 'fail': [], 'success': [] } }
@@ -19,10 +32,13 @@ app = Sammy('#main', function (sam) {
             }
             $('#flash').html(html);
         },
+
+        // API connection helper
         api: function (uri, callback, method, data) {
             method = typeof method !== 'undefined' ? method : 'GET';
             data   = typeof data   !== 'undefined' ? data   : {};
             auth   = "Basic "+ btoa(store.get('user') +':'+ atob(store.get('password')));
+            this.swap('<img src="img/ajax-loader.gif" />');
             jQuery.ajax({
                 url: store.get('url') + uri,
                 type: method,
@@ -45,33 +61,51 @@ app = Sammy('#main', function (sam) {
                 callback(result);
             });
         },
+
+        // Render view (cross-browser)
         view: function (view, data) {
-            //data = typeof data !== 'undefined' ? data : {};
             this.render('views/'+ view +'.ms', data).swap();
         }
     });
 
-    sam.after(function () {
-        store.set('flash', {'info': [], 'fail': [], 'success': [] });
-    });
 
+    /**
+     * Filters
+     *
+     */
     sam.before({except: {path: '#/login'}}, function (req) {
+
+        // Store path for further redirections
         store.set('path', req.path);
-        if (!store.get('user')) {
+
+        // Redirect to login page if no credentials are stored
+        if (!store.get('password')) {
             req.redirect('#/login');
             return false;
         }
+
+        // Clear flash display
+        if (!store.get('flash')) {
+            $('#flash').html('');
+        }
     });
 
+    sam.after(function () {
+
+        // Clear flash notifications
+        store.clear('flash');
+    });
+
+
+    /**
+     * Routes
+     *
+     * Note: var "c" is Sammy's route context
+     * @doc http://sammyjs.org/docs/api/#Sammy.EventContext
+     *
+     */
     sam.get('#/', function (c) {
         c.view('home');
-    });
-
-    sam.get('#/users/:user', function (c) {
-        c.swap('');
-        c.api('/users/'+ c.params['user'], function(data) {
-            c.view('user_info', data);
-        });
     });
 
     sam.get('#/login', function (c) {
@@ -80,7 +114,7 @@ app = Sammy('#main', function (sam) {
 
     sam.post('#/login', function (c) {
         store.set('url', c.params['url']);
-        store.set('user', c.params['user']);
+        store.set('user', 'admin');
         store.set('password', btoa(c.params['password']));
         c.api('/users', function(data) {
             if (data.error) {
@@ -95,9 +129,20 @@ app = Sammy('#main', function (sam) {
             }
         });
     });
+
+    sam.get('#/users/:user', function (c) {
+        c.swap('');
+        c.api('/users/'+ c.params['user'], function(data) {
+            c.view('user_info', data);
+        });
+    });
 });
 
 
+/**
+ * Run the app
+ *
+ */
 $(document).ready(function () {
     app.run('#/');
 });
