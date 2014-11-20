8d4ef07e9894a54b861a386e502f54abb5280b5e
opi <opi@no-log.org>
2014-11-20 10:40:49 +0100

    [enh] Edit user mail aliases & forward.

--- js/app.js
+++ js/app.js
@@ -332,6 +332,14 @@ app = Sammy('#main', function (sam) {
                 }
                 return 0;
             });
+        },
+
+        arrayDiff: function(arr1, arr2) {
+            arr1 = arr1 || [];
+            arr2 = arr2 || [];
+            return arr1.filter(function (a) {
+                return ((arr2.indexOf(a) == -1) && (a != ""));
+            });
         }
     });
 
@@ -705,13 +713,14 @@ app = Sammy('#main', function (sam) {
         c.api('/users/'+ c.params['user'], function(data) { // http://api.yunohost.org/#!/user/user_info_get_0
             c.api('/domains', function(dataDomains) { // http://api.yunohost.org/#!/domain/domain_list_get_2
 
+                // User email use a fake splitted field
                 email = data.mail.split('@');
                 data.email = {
                     username : email[0],
                     domain : email[1]
                 }
 
-
+                // Domains
                 data.domains = []
                 $.each(dataDomains.domains, function(key, value) {
                     data.domains.push({
@@ -727,50 +736,70 @@ app = Sammy('#main', function (sam) {
 
     // Update user information
     sam.put('#/users/:user', function (c) {
-        params = {}
+        // Get full user object
+        c.api('/users/'+ c.params['user'], function(user) {
 
-        // concat email/domain pseudo field
-        if (c.params['mail'] !== c.params['email'] + c.params['domain']) {
-            c.params['mail'] = c.params['email'] + c.params['domain'];
-        }
-        else {
-            c.params['mail'] = '';
-        }
-        c.params['email'] = '';
-        c.params['domain'] = '';
-
-        $.each(c.params.toHash(), function(key, value) {
-            if (value !== '' && key !== 'user') { params[key] = value; }
-        });
-
-        if ($.isEmptyObject(params)) {
-            c.flash('fail', y18n.t('error_modify_something'));
-            store.clear('slide');
-            // c.redirect('#/users/'+ c.params['user'] + '/edit');
-        } else {
-            if (params['password']) {
-                if (params['password'] == params['confirmation']) {
-                    if (params['password'].length < 4) {
-                        c.flash('fail', y18n.t('password_too_short'));
-                        store.clear('slide');
-                    }
-                    else {
-                        params['change_password'] = params['password'];
-                        c.api('/users/'+ c.params['user'], function(data) { // http://api.yunohost.org/#!/user/user_update_put_1
-                            c.redirect('#/users/'+ c.params['user']);
-                        }, 'PUT', params);
-                    }
-                } else {
-                    c.flash('fail', y18n.t('passwords_dont_match'));
-                    store.clear('slide');
-                }
+            // concat email/domain pseudo field
+            if (c.params['mail'] !== c.params['email'] + c.params['domain']) {
+                c.params['mail'] = c.params['email'] + c.params['domain'];
             }
             else {
-                c.api('/users/'+ c.params['user'], function(data) { // http://api.yunohost.org/#!/user/user_update_put_1
-                    c.redirect('#/users/'+ c.params['user']);
-                }, 'PUT', params);
+                c.params['mail'] = '';
             }
-        }
+            // Clear temporary inputs
+            c.params['email'] = c.params['domain'] = '';
+
+
+            // force array type for mail aliases and redirections
+            if (typeof c.params['mailalias'] == 'string') {c.params['mailalias'] = [c.params['mailalias']]};
+            if (typeof c.params['mailforward'] == 'string') {c.params['mailforward'] = [c.params['mailforward']]};
+
+            // Check for added/removed aliases and redirections
+            c.params['add_mailalias'] = c.arrayDiff(c.params['mailalias'], user['mail-aliases']);
+            c.params['remove_mailalias'] = c.arrayDiff(user['mail-aliases'], c.params['mailalias']);
+            c.params['add_mailforward'] = c.arrayDiff(c.params['mailforward'], user['mail-forward']);
+            c.params['remove_mailforward'] = c.arrayDiff(user['mail-forward'], c.params['mailforward']);
+
+            // Clear temporary inputs
+            c.params['mailalias'] = c.params['mailforward'] = '';
+
+            // Remove empty inputs
+            params = {}
+            $.each(c.params.toHash(), function(key, value) {
+                if (value.length > 0 && key !== 'user') { params[key] = value; }
+            });
+
+            if ($.isEmptyObject(params)) {
+                c.flash('fail', y18n.t('error_modify_something'));
+                store.clear('slide');
+                c.redirect('#/users/'+ c.params['user'] + '/edit');
+            } else {
+                if (params['password']) {
+                    if (params['password'] == params['confirmation']) {
+                        if (params['password'].length < 4) {
+                            c.flash('fail', y18n.t('password_too_short'));
+                            store.clear('slide');
+                            c.redirect('#/users/'+ c.params['user'] + '/edit');
+                        }
+                        else {
+                            params['change_password'] = params['password'];
+                            c.api('/users/'+ c.params['user'], function(data) { // http://api.yunohost.org/#!/user/user_update_put_1
+                                c.redirect('#/users/'+ c.params['user']);
+                            }, 'PUT', params);
+                        }
+                    } else {
+                        c.flash('fail', y18n.t('passwords_dont_match'));
+                        store.clear('slide');
+                        c.redirect('#/users/'+ c.params['user'] + '/edit');
+                    }
+                }
+                else {
+                    c.api('/users/'+ c.params['user'], function(data) { // http://api.yunohost.org/#!/user/user_update_put_1
+                        c.redirect('#/users/'+ c.params['user']);
+                    }, 'PUT', params);
+                }
+            }
+        }, 'GET');
     });
 
     // Remove existing user
