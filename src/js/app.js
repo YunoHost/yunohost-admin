6371685ec8aab43cbce7e394fe4074be50adbfc5
polytan02 <polytan02@users.noreply.github.com>
2015-03-16 11:41:43 +0000

    Reboot function in the admin/tools menu
    Reboot function (to be done !), to add the possibility of remotely rebooting the yunohost system.
    
    The function added is JUST AN EXAMPLE and needs to be done properly.

--- src/js/app.js
+++ src/js/app.js
@@ -1757,6 +1757,11 @@ app = Sammy('#main', function (sam) {
     sam.get('#/tools/ca', function (c) {
         c.view('tools/tools_ca');
     });
+    
+    // System Reboot
+    sam.get('#/tools/reboot_server', function (c) {
+        c.api('tools/reboot_server') {
+    });
 
     // Security feed
     sam.get('#/tools/security-feed', function (c) {
