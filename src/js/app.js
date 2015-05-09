4d2fe24ddb7c7cc16c815d8aab53a13ba39e088a
zamentur <valentin@grimaud.me>
2015-05-10 01:24:24 +0200

    Revert "[fix] Link broken on security alert  (#95)"
    This reverts commit d3c3063d2e447be1d31caee0cf0bd199c4df4e5e.

--- src/js/app.js
+++ src/js/app.js
@@ -435,18 +435,11 @@ app = Sammy('#main', function (sam) {
 
                 // Loop through items
                 $('item', xml).each(function(k, v) {
-                    var link=$('link', v)[0].innerHTML;
-                    if (typeof link == 'string' && link!='' && link.charAt(0)=='/')
-                        link=forumUrl+link;
-                    var description=$('description', v)[0].textContent;
-                    description=description.replace('href="/','href="'+forumUrl+'/');
-                    
                     var item = {
                         guid: $('guid', v)[0].innerHTML,
                         title: $('title', v)[0].innerHTML,
-                        url: link,
-                        desc: description,
-                        date: $('pubDate', v)[0].innerHTML.split(' +')[0],
+                        url: $('link', v)[0].innerHTML,
+                        desc: $('description', v)[0].textContent
                     }
                     if (viewedItems.indexOf(item.guid) === -1) {
                         // Show security message to administrator
@@ -1771,7 +1764,6 @@ app = Sammy('#main', function (sam) {
         };
 
         // Get security feed and display items
-        var forumUrl = 'https://forum.yunohost.org';
         var securityUrl = 'https://forum.yunohost.org/c/security';
         var securityFeed = 'https://yunohost.org/security.rss';
 
@@ -1786,19 +1778,14 @@ app = Sammy('#main', function (sam) {
             dataType: "xml"
         })
         .done(function(xml){
+
             // Loop through items
             $('item', xml).each(function(k, v) {
-                var link=$('link', v)[0].innerHTML;
-                if (typeof link == 'string' && link!='' && link.charAt(0)=='/')
-                    link=forumUrl+link;
-                var description=$('description', v)[0].textContent;
-                description=description.replace('href="/','href="'+forumUrl+'/');
-                
                 var item = {
                     guid: $('guid', v)[0].innerHTML,
                     title: $('title', v)[0].innerHTML,
-                    url: link,
-                    desc: description,
+                    url: $('link', v)[0].innerHTML,
+                    desc: $('description', v)[0].textContent,
                     date: $('pubDate', v)[0].innerHTML.split(' +')[0],
                 }
                 data.items.push(item);
