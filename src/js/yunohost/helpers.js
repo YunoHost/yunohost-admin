604a76759d181b041aa2cc82761f2d033fd23511
opi <opi@zeropi.net>
2016-05-23 20:55:25 +0200

    [fix] Remove useless 'move' CSS class.

--- src/js/yunohost/helpers.js
+++ src/js/yunohost/helpers.js
@@ -221,19 +221,19 @@
                 if (store.get('slide') == 'back') {
                     store.clear('slide');
                     $('#slideBack').css('display', 'none');
-                    $('#slider-container').removeClass('move').css('margin-left', '-100%');
+                    $('#slider-container').css('margin-left', '-100%');
                     $('#slideTo').show().html($('#main').html());
                     leSwap();
-                    $('#slider-container').addClass('move').css('margin-left', '0px');
+                    $('#slider-container').css('margin-left', '0px');
 
                 // Slide to effect
                 } else if (store.get('slide') == 'to') {
                     store.clear('slide');
                     $('#slideTo').css('display', 'none');
-                    $('#slider-container').removeClass('move').css('margin-left', '0px');
+                    $('#slider-container').css('margin-left', '0px');
                     $('#slideBack').show().html($('#main').html());
                     leSwap();
-                    $('#slider-container').addClass('move').css('margin-left', '-100%');
+                    $('#slider-container').css('margin-left', '-100%');
 
                 } else {
                     leSwap();
