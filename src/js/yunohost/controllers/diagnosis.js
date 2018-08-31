75ab8a3d7630e1714e0df06cfbd15d3c63dd1492
Alexandre Aubin <alex.aubin@mailoo.org>
2018-08-31 13:53:13 +0000

    Add recheck / details and ignore button to the view

--- src/js/yunohost/controllers/diagnosis.js
+++ src/js/yunohost/controllers/diagnosis.js
@@ -12,28 +12,34 @@
 
         // Why this method ?
         c.api('/diagnosis/show', function(data) { 
-		console.log(data);
-		c.view('diagnosis/diagnosis_show', data);
 		for (var i = 0 ; i < data.reports.length ; i++)
 		{
+		    // Convert timestamp to datetime
+		    data.reports[i].time = new Date(data.reports[i].timestamp*1000);
 		    for (var j = 0 ; j < data.reports[i].reports.length ; j++)
 		    {
 			var type_ = data.reports[i].reports[j].report[0];
 			type_ = type_.toLowerCase();
 			var icon = "";
+			var issue = false;
+
 			if (type_ == "success") {
 			    icon = "check-circle";
 			}
 			else if (type_ == "warning") {
 			    icon = "warning";
+			    issue = true;
 			}
 			else if (type_ == "error") {
 			    icon = "times";
+			    issue = true;
 			}
 			data.reports[i].reports[j].report[0] = type_;
-			data.reports[i].reports[j].report.push(icon);
+			data.reports[i].reports[j].icon = icon;
+			data.reports[i].reports[j].issue = issue;
 		    };
 		};
+		c.view('diagnosis/diagnosis_show', data);
         }, 'GET');
 
     });
