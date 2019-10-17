c2810a1dcb1a46a65413a83ac244dbb91684f36c
Alexandre Aubin <alex.aubin@mailoo.org>
2019-10-17 19:04:59 +0200

    Fix an issue where click events where pilling up if clicking outside the modal window

--- src/js/yunohost/helpers.js
+++ src/js/yunohost/helpers.js
@@ -323,8 +323,13 @@
             // Modal content
             $('.content', box).html(content);
 
+            // Clear any remaining click event that could still be there (e.g.
+            // clicking outside the modal window doesn't equal to clicking
+            // cancel...
+            $('#modal footer button').unbind( "click" );
+
             // Handle buttons
-            $('footer button', box)
+            $('#modal footer button', box)
                 .click(function(e){
                     e.preventDefault();
 
