d7e7ba30ba528c9842050894d97e9d2137267eed
Kay0u <pierre@kayou.io>
2020-06-03 23:02:39 +0200

    Gulp Upgrade

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,6 +6,7 @@ var concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     csslint = require('gulp-csslint'),
     jshint = require('gulp-jshint'),
+    mustache = require('gulp-mustache'),
     less = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
     cssmin = require('gulp-cssmin'),
@@ -19,35 +20,18 @@ var concat = require('gulp-concat'),
 isDev = (util.env.dev) ? true : false;
 isProduction = !isDev;
 
-// Global build task
-gulp.task('build', [
-    'css',
-    'fonts',
-    'js',
-    'img',
-]);
-
-
-// Watch task
-gulp.task('watch', function(){
-    gulp.watch('js/**/*.js', ['js']);
-    gulp.watch('css/*.less', ['css']);
-});
-
-
 // JS task
 gulp.task('js', function() {
     return gulp.src([
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/js-cookie/src/js.cookie.js',
-            'bower_components/handlebars/handlebars.js',
-            'bower_components/handlebars-helper-intl/dist/handlebars-intl-with-locales.js',
-            'bower_components/sammy/lib/sammy.js',
-            'bower_components/sammy/lib/plugins/sammy.handlebars.js',
-            'bower_components/sammy/lib/plugins/sammy.json.js',
-            'bower_components/sammy/lib/plugins/sammy.storage.js',
-            'bower_components/bootstrap/dist/js/bootstrap.js',
-            'bower_components/isotope-layout/dist/isotope.pkgd.js',
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/handlebars/dist/handlebars.js',
+            'node_modules/handlebars-intl/dist/handlebars-intl-with-locales.js',
+            'node_modules/sammy/lib/sammy.js',
+            'node_modules/sammy/lib/plugins/sammy.handlebars.js',
+            'node_modules/sammy/lib/plugins/sammy.json.js',
+            'node_modules/sammy/lib/plugins/sammy.storage.js',
+            'node_modules/bootstrap/dist/js/bootstrap.js',
+            'node_modules/isotope-layout/dist/isotope.pkgd.js',
             'js/yunohost/y18n.js',
             'js/yunohost/main.js',
             'js/yunohost/helpers.js',
@@ -71,17 +55,17 @@ gulp.task('js-lint', function() {
 // Fonts
 gulp.task('fonts', function() {
     return gulp.src([
-            'bower_components/fork-awesome/fonts/*',
-            'bower_components/source-code-pro/EOT/*.eot',
-            'bower_components/source-code-pro/OTF/*.otf',
-            'bower_components/source-code-pro/TTF/*.ttf',
-            'bower_components/source-code-pro/WOFF/OTF/*.woff',
-            'bower_components/source-code-pro/WOFF2/TTF/*.woff2',
-            'bower_components/source-sans-pro/EOT/*.eot',
-            'bower_components/source-sans-pro/OTF/*.otf',
-            'bower_components/source-sans-pro/TTF/*.ttf',
-            'bower_components/source-sans-pro/WOFF/OTF/*.woff',
-            'bower_components/source-sans-pro/WOFF2/TTF/*.woff2',
+            'node_modules/fork-awesome/fonts/*',
+            'node_modules/source-code-pro/EOT/*.eot',
+            'node_modules/source-code-pro/OTF/*.otf',
+            'node_modules/source-code-pro/TTF/*.ttf',
+            'node_modules/source-code-pro/WOFF/OTF/*.woff',
+            'node_modules/source-code-pro/WOFF2/TTF/*.woff2',
+            'node_modules/source-sans-pro/EOT/*.eot',
+            'node_modules/source-sans-pro/OTF/*.otf',
+            'node_modules/source-sans-pro/TTF/*.ttf',
+            'node_modules/source-sans-pro/WOFF/OTF/*.woff',
+            'node_modules/source-sans-pro/WOFF2/TTF/*.woff2',
         ])
         .pipe(gulp.dest('./dist/fonts'))
 });
@@ -115,4 +99,21 @@ gulp.task('img', function () {
     return gulp.src('img/*')
         .pipe(gulpif(isProduction, imagemin()))
         .pipe(gulp.dest('./dist/img'))
+});
+
+// Views task
+gulp.task('views', function () {
+    return gulp.src('views/**/*.ms')
+    .pipe(gulp.dest("./dist/views"));
+});
+
+// Global build task
+gulp.task('build', gulp.series('css', 'fonts', 'js', 'img', 'views'));
+
+
+// Watch task
+gulp.task('watch', function() {
+    gulp.watch('js/**/*.js', gulp.series('js'));
+    gulp.watch('css/*.less', gulp.series('css'));
+    gulp.watch('views/**/*.ms', gulp.series('views'));
 });
