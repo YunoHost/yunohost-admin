269dcf18525a2441450f02a0bd7b12bc0b5ee97e
Axolotle <nchesnais@mailoo.org>
2020-07-07 21:59:29 +0200

    add Home view and ListGroupItem component to display main menu items

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Yunohost logo" src="../assets/logo.png">
-  </div>
+    <div class="home">
+        <b-list-group>
+            <list-group-item
+                v-for="item in menu"
+                v-bind:key="item.id"
+                v-bind:uri="item.uri"
+                v-bind:icon="item.icon"
+            >{{ $t(item.translation) }}</list-group-item>
+        </b-list-group>
+    </div>
 </template>
 
 <script>
+import ListGroupItem from '@/components/ListGroupItem'
+
 export default {
     name: 'Home',
+    data: () => {
+        return {
+            menu: [
+                {id: 0, uri: '/users', icon: 'users', translation: 'users'},
+                {id: 1, uri: '/domains', icon: 'globe', translation: 'domains'},
+                {id: 2, uri: '/apps', icon: 'cubes', translation: 'applications'},
+                {id: 3, uri: '/update', icon: 'refresh', translation: 'system_update'},
+                {id: 4, uri: '/services', icon: 'cog', translation: 'services'},
+                {id: 5, uri: '/tools', icon: 'wrench', translation: 'tools'},
+                {id: 6, uri: '/diagnosis', icon: 'stethoscope', translation: 'diagnosis'},
+                {id: 7, uri: '/backup', icon: 'archive', translation: 'backup'},
+            ]
+        }
+    },
+    components: {
+        ListGroupItem
+    }
 }
 </script>
