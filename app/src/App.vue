aa19c56933fca0f41d1ea2af0f35819802834b43
Axolotle <nchesnais@mailoo.org>
2020-07-08 14:35:22 +0200

    add icon component & add global import of some components

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,10 +5,14 @@
                 <b-navbar-brand to="/" exact exact-active-class="active"><img alt="Yunohost logo" src="./assets/logo.png"></b-navbar-brand>
                 <b-navbar-nav class="ml-auto">
                     <li class="nav-item">
-                        <b-button href="/yunohost/sso" variant="primary" block size="sm">{{ $t('user_interface_link') }}</b-button>
+                        <b-button href="/yunohost/sso" variant="primary" block size="sm">
+                            {{ $t('user_interface_link') }} <icon iname="user" class="sm"/>
+                        </b-button>
                     </li>
                     <li class="nav-item">
-                        <b-button to="/logout" variant="outline-dark" block size="sm">{{ $t('logout') }}</b-button>
+                        <b-button to="/logout" variant="outline-dark" block size="sm">
+                            {{ $t('logout') }} <icon iname="sign-out" class="sm"/>
+                        </b-button>
                     </li>
                 </b-navbar-nav>
             </b-navbar>
@@ -21,9 +25,15 @@
         <footer>
             <nav>
                 <b-nav>
-                    <b-nav-item href="https://yunohost.org/docs" target="_blank" link-classes='text-secondary'>Documentation</b-nav-item>
-                    <b-nav-item href="https://yunohost.org/help" target="_blank" link-classes='text-secondary'>Need help?</b-nav-item>
-                    <b-nav-item href="https://donate.yunohost.org/" target="_blank" link-classes='text-secondary'>Donate</b-nav-item>
+                    <b-nav-item href="https://yunohost.org/docs" target="_blank" link-classes='text-secondary'>
+                        <icon iname="book" class="sm"/> Documentation
+                    </b-nav-item>
+                    <b-nav-item href="https://yunohost.org/help" target="_blank" link-classes='text-secondary'>
+                        <icon iname="life-ring" class="sm"/> Need help?
+                    </b-nav-item>
+                    <b-nav-item href="https://donate.yunohost.org/" target="_blank" link-classes='text-secondary'>
+                        <icon iname="heart" class="sm"/> Donate
+                    </b-nav-item>
                     <b-nav-text class="ml-auto" id="yunohost-version">version</b-nav-text>
                 </b-nav>
             </nav>
@@ -52,9 +62,13 @@ header {
             li {
                 margin: .2rem 0;
             }
+            icon {
+                margin-left: .5rem;
+            }
         }
     }
 }
+
 
 footer {
     padding: 1rem 0;
--- app/src/main.js
+++ app/src/main.js
@@ -4,11 +4,19 @@ import './plugins/bootstrap-vue'
 import i18n from './plugins/i18n'
 import router from './plugins/router'
 
+import * as globalsComponents from './components/globals'
+
 
 Vue.config.productionTip = false
+
+// Register global components
+for (let component of Object.values(globalsComponents)) {
+     Vue.component(component.name, component)
+}
+
 
 new Vue({
     i18n,
     router,
-    render: h => h(App)
+    render: h => h(App),
 }).$mount('#app')
