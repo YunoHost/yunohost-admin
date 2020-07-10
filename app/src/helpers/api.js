0334515a43e3ccaf19c86f6e0b7b595ea260bf57
Axolotle <nchesnais@mailoo.org>
2020-07-10 18:40:28 +0200

    add temp basic api login and logout methods

---
+++ app/src/helpers/api.js
@@ -0,0 +1,38 @@
+
+
+function objectToParams(object) {
+    const urlParams = new URLSearchParams();
+    for (const [key, value] of Object.entries(object)) {
+        urlParams.append(key, value)
+    }
+    return urlParams
+}
+
+
+export default {
+    options: {
+        credentials: 'include',
+        mode: 'cors',
+        headers: {
+            // FIXME is it important to keep this previous `Accept` header ?
+            // 'Accept': 'application/json, text/javascript, */*; q=0.01',
+            // Auto header is :
+            // "Accept": "*/*",
+
+            // Also is this still important ? (needed by back-end)
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+    },
+
+    login(password) {
+        return fetch('/api/login', {
+            method: 'POST',
+            body: objectToParams({password}),
+            ...this.options
+        }).then(response => (response.ok))
+    },
+
+    logout() {
+        return fetch('/api/logout', this.options).then(response => (response.ok))
+    }
+}
