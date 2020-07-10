b63e0334a97aece4ba2f255ba5a3cb35b21dd363
Axolotle <nchesnais@mailoo.org>
2020-07-10 18:38:59 +0200

    add vuex store with 'connected' state

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './plugins/bootstrap-vue'
 import i18n from './plugins/i18n'
 import router from './plugins/router'
+import store from './plugins/store'
 
 import * as globalsComponents from './components/globals'
 
@@ -11,12 +12,12 @@ Vue.config.productionTip = false
 
 // Register global components
 for (let component of Object.values(globalsComponents)) {
-     Vue.component(component.name, component)
+    Vue.component(component.name, component)
 }
-
 
 new Vue({
     i18n,
     router,
+    store,
     render: h => h(App),
 }).$mount('#app')
