d9647eabf55553aadbd6133581908c6bf8a71b35
Axolotle <nchesnais@mailoo.org>
2020-07-14 19:51:25 +0200

    add Users view and route

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,11 +1,15 @@
 import Home from './views/Home'
 import Login from './views/Login'
+import Users from './views/Users'
 
 
 const routes = [
     {name: 'home', path: '/', component: Home},
     {name: 'login', path: '/login', component: Login, meta: {
         noAuth: true
+    }},
+    {name: 'users', path: '/users', component: Users, meta: {
+        breadcrumb: [{name: 'users', trad: 'users'}]
     }},
 ]
 
