1812ebbac8b84371b662b1b687d2cbcbc5b2fee7
Axolotle <nchesnais@mailoo.org>
2020-07-15 17:30:09 +0200

    add index to import all user related views and modify some routes name

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <b-list-group>
-      <b-list-group-item v-for="item in menu" :key="item.id" :to="item.uri">
+      <b-list-group-item
+        v-for="item in menu"
+        :key="item.id"
+        :to="{name: item.routeName}"
+      >
         <icon :iname="item.icon" class="lg" />
         <h2>{{ $t(item.translation) }}</h2>
         <icon iname="chevron-right" class="lg fs-sm ml-auto" />
@@ -16,14 +20,14 @@ export default {
   data: () => {
     return {
       menu: [
-        { id: 0, uri: '/users', icon: 'users', translation: 'users' },
-        { id: 1, uri: '/domains', icon: 'globe', translation: 'domains' },
-        { id: 2, uri: '/apps', icon: 'cubes', translation: 'applications' },
-        { id: 3, uri: '/update', icon: 'refresh', translation: 'system_update' },
-        { id: 4, uri: '/services', icon: 'cog', translation: 'services' },
-        { id: 5, uri: '/tools', icon: 'wrench', translation: 'tools' },
-        { id: 6, uri: '/diagnosis', icon: 'stethoscope', translation: 'diagnosis' },
-        { id: 7, uri: '/backup', icon: 'archive', translation: 'backup' }
+        { id: 0, routeName: 'user-list', icon: 'users', translation: 'users' },
+        { id: 1, routeName: 'domains', icon: 'globe', translation: 'domains' },
+        { id: 2, routeName: 'apps', icon: 'cubes', translation: 'applications' },
+        { id: 3, routeName: 'update', icon: 'refresh', translation: 'system_update' },
+        { id: 4, routeName: 'services', icon: 'cog', translation: 'services' },
+        { id: 5, routeName: 'tools', icon: 'wrench', translation: 'tools' },
+        { id: 6, routeName: 'diagnosis', icon: 'stethoscope', translation: 'diagnosis' },
+        { id: 7, routeName: 'backup', icon: 'archive', translation: 'backup' }
       ]
     }
   }
---
+++ app/src/views/user/index.js
@@ -0,0 +1,2 @@
+export { default as User } from './User'
+export { default as UserList } from './UserList'
