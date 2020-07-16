17350671fd936d0a97299acd45b8120e583c7cde
Axolotle <nchesnais@mailoo.org>
2020-07-16 16:30:19 +0200

    add users data call/storing

--- app/src/helpers/dataStore.js
+++ app/src/helpers/dataStore.js
@@ -2,17 +2,23 @@ import api from './api'
 
 export default {
   state: () => ({
-    domains: undefined
+    domains: undefined,
+    users: undefined
   }),
   mutations: {
-    'SET_DATA' (state, { key, data }) {
-      state[key] = data
+    'SET_DOMAINS' (state, domains) {
+      state.domains = domains
+    },
+    'SET_USERS' (state, users) {
+      console.log(users)
+      state.users = Object.keys(users).length === 0 ? null : Object.values(users)
     }
   },
   actions: {
     async 'FETCH' ({ commit }, uri) {
-      return api.get('/' + uri).then(data => {
-        commit('SET_DATA', { data: data[uri], key: uri })
+      return api.get(uri).then(responseData => {
+        const data = responseData[uri] ? responseData[uri] : responseData
+        commit('SET_' + uri.toUpperCase(), data)
       })
     }
   },
