70ff47de9860fbae3f394cf1949b3189a7dfe1a6
Axolotle <nchesnais@mailoo.org>
2020-07-27 20:46:27 +0200

    add UserEdit and api PUT

--- app/src/views/user/index.js
+++ app/src/views/user/index.js
@@ -1,3 +1,4 @@
 export { default as UserList } from './UserList'
 export { default as UserCreate } from './UserCreate'
 export { default as User } from './User'
+export { default as UserEdit } from './UserEdit'
