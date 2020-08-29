167407e192a83b8ddb862cd6566be47ace98f223
Axolotle <nchesnais@mailoo.org>
2020-08-29 14:14:09 +0200

    add date-fns dep & update deps & optimize date-fns locales imports

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,41 @@
+const webpack = require('webpack')
+
+const dateFnsLocales = [
+  'ar',
+  'bn', // for 'bn_BD'
+  'ca', // for 'ca' & 'oc'
+  'de',
+  'el',
+  'en-GB', // for 'en' & 'ne'
+  'eo',
+  'es',
+  'eu',
+  'fr', // for 'fr' & 'br'
+  'hi',
+  'hu',
+  'it',
+  'nb', // for 'nb_NO'
+  'nl',
+  'oc',
+  'pl',
+  'pt',
+  'ru',
+  'sv',
+  'tr',
+  'zh_CN' // for 'zh_Hans'
+]
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      // Will limit the available locales so webpack won't generate chunks for every
+      // locales found in `date-fns/locales/` but only those listed.
+      new webpack.ContextReplacementPlugin(
+        /date-fns[/\\]/,
+        new RegExp(`[/\\\\](${dateFnsLocales.join('|')})[/\\\\]index.js$`)
+      )
+    ]
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -17,7 +54,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: process.env.API_URL
+        target: `https://${process.env.VUE_APP_IP}/yunohost`
       }
     }
   }
