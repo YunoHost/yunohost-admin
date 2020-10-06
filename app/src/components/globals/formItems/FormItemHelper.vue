7a5de28833db26c6d61029649f9c56b994760e27
Axolotle <nchesnais@mailoo.org>
2020-10-06 23:48:48 +0200

    add basic components to be used from props

---
+++ app/src/components/globals/formItems/FormItemHelper.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-form-group
+    label-cols="0" label-class="font-weight-bold" class="mb-4"
+    :label="label" :label-for="'form-item-' + props.id"
+  >
+    <slot name="default">
+      <component
+        :is="component" v-bind="props"
+        v-model="props.value" @input="test"
+      />
+    </slot>
+
+    <template v-if="description || example || link" v-slot:description>
+      <div class="d-flex">
+        <span v-if="example">{{ $t('form_input_example', { example }) }}</span>
+
+        <b-link v-if="link" :to="link" class="ml-auto">
+          {{ link.text }}
+        </b-link>
+      </div>
+
+      <template v-if="description">
+        {{ description }}
+      </template>
+    </template>
+
+    <b-form-invalid-feedback v-if="'isValid' in props" :id="props.id + '-feedback'" :state="props.value.isValid">
+      {{ props.error }}
+    </b-form-invalid-feedback>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'FormItemHelper',
+
+  props: {
+    component: { type: String, default: 'InputItem' },
+    props: { type: Object, required: true },
+    label: { type: String, required: true },
+    description: { type: String, default: null },
+    example: { type: String, default: null },
+    link: { type: Object, default: null }
+  },
+
+  data () {
+    return {
+      content: this.value
+    }
+  },
+
+  methods: {
+    test () {
+      console.log(this.props.value)
+      // this.props.isValid = false
+    }
+  }
+}
+</script>
--- app/src/components/globals/index.js
+++ app/src/components/globals/index.js
@@ -1,2 +1,3 @@
 export { default as Icon } from './Icon'
 export { default as Breadcrumb } from './Breadcrumb'
+export { FormItemHelper, InputItem, SelectItem, CheckboxItem } from './formItems'
