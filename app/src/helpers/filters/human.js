9caac2439f10e44e96013914b0f125f33f1848ab
Axolotle <nchesnais@mailoo.org>
2020-11-03 16:30:08 +0100

    update AppList & AppInfo with permission labels

--- app/src/helpers/filters/human.js
+++ app/src/helpers/filters/human.js
@@ -4,3 +4,10 @@ export function humanSize (bytes) {
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
 }
+
+
+export function humanPermissionName (text) {
+  return text.split('.')[1].replace('_', ' ').replace(/\w\S*/g, part => {
+    return part.charAt(0).toUpperCase() + part.substr(1).toLowerCase()
+  })
+}
