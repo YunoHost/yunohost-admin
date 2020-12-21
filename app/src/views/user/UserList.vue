98a956916af678c44f331229fb9b3eb6ccefd14a
ljf <ljf+git@grimaud.me>
2020-12-21 06:14:24 +0100

    [enh] Add Import and export button

--- app/src/views/user/UserList.vue
+++ app/src/views/user/UserList.vue
@@ -13,10 +13,22 @@
           {{ $t('groups_and_permissions_manage') }}
         </b-button>
 
-        <b-button variant="success" :to="{name: 'user-create'}">
-          <icon iname="plus" />
-          {{ $t('users_new') }}
-        </b-button>
+        <div class="btn-group">
+          <b-dropdown split class="m-2" variant="success"
+                      :split-to="{name: 'user-create'}">
+            <template #button-content>
+              <span aria-hidden="true" class="icon fa fa-plus"></span>
+              {{ $t('users_new') }}
+            </template>
+            <b-dropdown-item :to="{name: 'user-import'}">
+              <icon iname="plus" /> {{ $t('users_import') }}
+            </b-dropdown-item>
+            <b-dropdown-item @click="downloadExport">
+              <icon iname="download" /> {{ $t('users_export') }}
+            </b-dropdown-item>
+          </b-dropdown>
+        </div>
+
       </div>
     </div>
 
@@ -57,6 +69,12 @@ export default {
   data: function () {
     return {
       search: ''
+    }
+  },
+  methods: {
+    downloadExport () {
+      const host = this.$store.getters.host
+      window.open(`https://${host}/yunohost/api/users/export`, '_blank')
     }
   },
   computed: {
