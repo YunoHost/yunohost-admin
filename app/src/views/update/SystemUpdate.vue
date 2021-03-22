227e359e42ab7c85d4cc50b983486e9a62a062ac
axolotle <nchesnais@mailoo.org>
2021-03-22 22:07:03 +0100

    update upgrade api routes

--- app/src/views/update/SystemUpdate.vue
+++ app/src/views/update/SystemUpdate.vue
@@ -74,7 +74,7 @@ export default {
     return {
       queries: [
         ['GET', 'migrations?pending'],
-        ['PUT', 'update']
+        ['PUT', 'update/all']
       ],
       // API data
       migrationsNotDone: undefined,
@@ -95,10 +95,7 @@ export default {
       const confirmed = await this.$askConfirmation(confirmMsg)
       if (!confirmed) return
 
-      const uri = type === 'specific_app'
-        ? 'upgrade/apps?app=' + id
-        : 'upgrade?' + type
-
+      const uri = id !== null ? `apps/${id}/upgrade` : 'upgrade/' + type
       api.put(uri).then(() => {
         this.$router.push({ name: 'tool-logs' })
       })
