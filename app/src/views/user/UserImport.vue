67cf6f46d8b174efe142ae50a7a3de7cd4ffbb3b
axolotle <nchesnais@mailoo.org>
2021-09-19 21:48:38 +0200

    fix UserImport missing await for file handling

--- app/src/views/user/UserImport.vue
+++ app/src/views/user/UserImport.vue
@@ -87,7 +87,7 @@ export default {
       Object.assign(requestArgs, this.form)
       if (!requestArgs.delete) delete requestArgs.delete
       if (!requestArgs.update) delete requestArgs.update
-      const data = formatFormData(requestArgs)
+      const data = await formatFormData(requestArgs)
       api.post('users/import', data, { asFormData: true }).then(() => {
         // Reset all cached data related to users.
         this.$store.dispatch('RESET_CACHE_DATA', ['users', 'users_details', 'groups', 'permissions'])
