72fe24b6afde3d9348c4f4fb1e5cf58f619699b5
axolotle <nchesnais@mailoo.org>
2022-12-15 13:25:06 +0100

    add prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,44 +1,13 @@
 module.exports = {
   root: true,
   env: {
-    es2021: true,
+    es2021: true
   },
   extends: [
-    'plugin:vue/strongly-recommended',
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'eslint-config-prettier'
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
-
-    'vue/max-attributes-per-line': [
-      'error',
-      {
-        singleline: 3,
-        multiline: 3,
-      }
-    ],
-    'vue/no-v-for-template-key': 'off',
-    'no-console': 'warn',
-    'template-curly-spacing': 'off',
-    'camelcase': 'off',
-    'indent': 'off',
-    'no-irregular-whitespace': 'off',
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 2,
-      }
-    ],
-    'no-unused-vars': [
-      'warn',
-      {
-        varsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-      }
-    ],
-    'quotes': [
-      'warn',
-      'single'
-    ],
-  },
+  }
 }
