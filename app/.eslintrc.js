4d346a8d13885f0c99844e951b6ddf9510e67a05
axolotle <nchesnais@mailoo.org>
2023-03-24 20:50:05 +0100

    separate eslint config from package.json

---
+++ app/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
+    'standard',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 3,
+        multiline: 3,
+      },
+    ],
+    'no-console': 'warn',
+    'template-curly-spacing': 'off',
+    camelcase: 'warn',
+    indent: 'off',
+    'no-irregular-whitespace': 'off',
+    'no-unused-vars': 'warn',
+    quotes: 'warn',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 2,
+      },
+    ],
+  },
+}
