fb0ec742f58986dfeb3f16acfdc9ff9a8c8813ca
axolotle <nchesnais@mailoo.org>
2024-02-24 17:54:16 +0100

    chore: replace standard with prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -7,32 +7,12 @@ module.exports = {
   extends: [
     'plugin:vue/strongly-recommended',
     'eslint:recommended',
-    'standard'
+    'plugin:prettier/recommended',
   ],
   rules: {
-    'vue/max-attributes-per-line': [
-      'error',
-      {
-        singleline: 3,
-        multiline: 3
-      }
-    ],
-    'vue/multi-word-component-names': 'off', // FIXME this should be adressed at some point
-    'no-console': 'warn',
-    'template-curly-spacing': 'off',
-    camelcase: 'warn',
-    indent: 'off',
-    'no-irregular-whitespace': 'off',
     'no-unused-vars': [
       'warn',
-      { varsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }
+      { varsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
     ],
-    quotes: 'warn',
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 2
-      }
-    ]
-  }
+  },
 }
