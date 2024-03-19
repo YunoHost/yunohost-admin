cbea05fa9b23b1e03309602665182d6b2cbf8112
axolotle <nchesnais@mailoo.org>
2024-03-19 20:22:19 +0100

    lint

--- app/src/helpers/yunohostArguments.js
+++ app/src/helpers/yunohostArguments.js
@@ -426,11 +426,11 @@ export function formatYunoHostConfigPanels(data) {
       const section = {
         id: _section.id,
         isActionSection: _section.is_action_section,
-        visible: _section.visible
+        visible: _section.visible,
       }
       if (_section.help) section.help = formatI18nField(_section.help)
       if (_section.name) section.name = formatI18nField(_section.name)
-      if (typeof _section.visible === "string") {
+      if (typeof _section.visible === 'string') {
         addEvaluationGetter('visible', section, section.visible, result.forms)
       }
 
