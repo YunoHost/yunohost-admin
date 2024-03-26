8479fe4a20babd9874a49cfef67e744adc85763a
axolotle <nchesnais@mailoo.org>
2024-03-26 23:17:17 +0100

    fix: use api instead of store action to query domains

--- app/src/store/info.js
+++ app/src/store/info.js
@@ -145,7 +145,7 @@ export default {
       // and login prompt will be shown automaticly
       await dispatch('GET_YUNOHOST_INFOS')
       commit('SET_CONNECTED', true)
-      await dispatch('GET', { uri: 'domains', storeKey: 'domains' })
+      await api.get({ uri: 'domains', storeKey: 'domains' })
     },
 
     RESET_CONNECTED({ commit }) {
