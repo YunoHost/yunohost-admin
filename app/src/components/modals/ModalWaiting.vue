f0bd35e2b1ef927f1baa69cbbde1c55cd3d9d5a2
axolotle <nchesnais@mailoo.org>
2024-08-05 17:14:34 +0200

    refactor: rework ModalWaiting to new requests

--- app/src/components/modals/ModalWaiting.vue
+++ app/src/components/modals/ModalWaiting.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-import MessageListGroup from '@/components/MessageListGroup.vue'
-import type { Obj } from '@/types/commons'
+import ModalOverlay from '@/components/modals/ModalOverlay.vue'
+import type { APIRequest } from '@/composables/useRequests'
 
 const props = defineProps<{
-  request: Obj
+  request: APIRequest
 }>()
 
-const hasMessages = computed(() => {
-  return props.request.messages && props.request.messages.length > 0
+const messages = computed(() => {
+  const messages = props.request.action?.messages
+  return messages?.length ? messages : null
 })
 
 const progress = computed(() => {
-  const progress = props.request.progress
+  const progress = props.request.action?.progress
   if (!progress) return null
   return {
     values: progress,
@@ -23,29 +24,26 @@ const progress = computed(() => {
 </script>
 
 <template>
-  <!-- This card receives style from `ViewLockOverlay` if used inside it -->
-  <BCardBody>
-    <BCardTitle
+  <ModalOverlay :request="request">
+    <h5
+      v-t="messages || progress ? 'api.processing' : 'api.waiting'"
       class="text-center mt-4"
-      v-t="hasMessages ? 'api.processing' : 'api_waiting'"
     />
 
-    <!-- PROGRESS BAR -->
-    <BProgress v-if="progress" class="my-4" :max="progress.max" height=".5rem">
+    <BProgress v-if="progress" :max="progress.max" height=".5rem" class="my-4">
       <BProgressBar variant="success" :value="progress.values[0]" />
       <BProgressBar variant="warning" :value="progress.values[1]" animated />
       <BProgressBar variant="secondary" :value="progress.values[2]" striped />
     </BProgress>
-    <!-- OR SPINNER -->
     <YSpinner v-else class="my-4" />
 
     <MessageListGroup
-      v-if="hasMessages"
-      :messages="request.messages"
+      v-if="messages"
+      auto-scroll
       bordered
       fixed-height
-      auto-scroll
       :limit="100"
+      :messages="messages"
     />
-  </BCardBody>
+  </ModalOverlay>
 </template>
