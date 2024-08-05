ab2c8a96918af9b2a06c3ab2f174365eb0474ab5
axolotle <nchesnais@mailoo.org>
2024-08-05 16:57:51 +0200

    chore: move requests modals to /components

--- app/src/views/_partials/index.ts
+++ app/src/views/_partials/index.ts
@@ -1,9 +1,2 @@
-export { default as ErrorDisplay } from './ErrorDisplay.vue'
-export { default as WarningDisplay } from './WarningDisplay.vue'
-export { default as WaitingDisplay } from './WaitingDisplay.vue'
-export { default as ReconnectingDisplay } from './ReconnectingDisplay.vue'
-
 export { default as HistoryConsole } from './HistoryConsole.vue'
-export { default as ViewLockOverlay } from './ViewLockOverlay.vue'
-
 export { default as DomainForm } from './DomainForm.vue'
