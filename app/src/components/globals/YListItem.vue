a5ff308a55f2ac8e2556c27a502d8bb8a1fafc02
Alexandre Aubin <alex.aubin@mailoo.org>
2025-02-18 16:19:47 +0100

    apps: add app logo in app list and app info

--- app/src/components/globals/YListItem.vue
+++ app/src/components/globals/YListItem.vue
@@ -4,10 +4,12 @@ withDefaults(
     label: string
     sublabel?: string
     description?: string
+    imageSrc?: string
   }>(),
   {
     sublabel: undefined,
     description: undefined,
+    imageSrc: undefined,
   },
 )
 
@@ -20,22 +22,33 @@ const slots = defineSlots<{
   <BListGroupItem
     class="d-flex justify-content-between align-items-center pe-0"
   >
-    <div>
-      <h5>
-        <strong class="fw-bold">{{ label }}</strong>
-        <small v-if="sublabel" class="ms-1 text-secondary">
-          {{ sublabel }}
-        </small>
-      </h5>
-      <p v-if="description || slots.default" class="m-0">
-        <slot name="default">
-          {{ description }}
-        </slot>
-      </p>
+    <div class="d-flex">
+      <div v-if="imageSrc" class="me-3">
+        <img :src="imageSrc" />
+      </div>
+      <div>
+        <h5>
+          <strong class="fw-bold">{{ label }}</strong>
+          <small v-if="sublabel" class="ms-1 text-secondary">
+            {{ sublabel }}
+          </small>
+        </h5>
+        <p v-if="description || slots.default" class="m-0">
+          <slot name="default">
+            {{ description }}
+          </slot>
+        </p>
+      </div>
     </div>
 
     <YIcon iname="chevron-right" class="lg fs-sm ms-auto" />
   </BListGroupItem>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  width: 2.5rem;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
