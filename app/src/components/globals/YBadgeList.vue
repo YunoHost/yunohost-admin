29a82fc7cf883af102e8701d574a5be9eb41f3ab
Alexandre Aubin <alex.aubin@mailoo.org>
2025-08-14 19:47:52 +0200

    Add YBadgeList component

---
+++ app/src/components/globals/YBadgeList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  badges: string[],
+  icon?: string,
+}>()
+
+const slots = defineSlots<{
+  default?: any
+}>()
+</script>
+
+<template>
+    <ul class="list-inline d-inline-block mb-2">
+      <li
+        v-for="badge in badges"
+        :key="id + '-' + badge"
+        class="list-inline-item"
+      >
+        <BBadge class="ms-2" pill>
+          <YIcon v-if="icon" :iname="icon" /> {{ badge }}
+        </BBadge>
+      </li>
+    </ul>
+</template>
