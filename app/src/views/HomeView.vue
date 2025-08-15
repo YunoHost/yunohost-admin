d197c049317ce302fa06b461a9d5372631f14b19
Alexandre Aubin <alex.aubin@mailoo.org>
2025-08-15 17:20:20 +0200

    Add quick info badges to home page

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import api from '@/api'
 const menu = [
   { routeName: 'user-list', icon: 'users', translation: 'users' },
   {
@@ -17,6 +18,43 @@ const menu = [
   },
   { routeName: 'backup', icon: 'archive', translation: 'backup' },
 ]
+
+const { apps_badges, upgrades_badges, diagnosis_badges } = await api
+  .get({ uri: 'dash', initial: true})
+  .then(({ apps, upgrades, diagnosis }) => {
+
+    let apps_badges: BadgeInfos[] = Array();
+    if (apps.notifications > 0) {
+        apps_badges.push({text: apps.notifications + ' notifications', variant: "info", icon: 'info-circle'})
+    }
+
+    let upgrades_badges: BadgeInfos[] = Array();
+    if (upgrades.pending_migrations > 0)
+    {
+        upgrades_badges.push({text: upgrades.pending_migrations + ' pending migrations', variant: "info", icon: "forward" })
+    }
+    if (upgrades.system_packages > 0)
+    {
+        upgrades_badges.push({text: upgrades.system_packages + ' system upgrades', variant: "info", icon: "server" })
+    }
+    if (upgrades.pending_migrations > 0)
+    {
+        upgrades_badges.push({text: upgrades.apps + ' app upgrades', variant: "info", icon: "cubes" })
+    }
+
+    let diagnosis_badges: BadgeInfos[] = Array();
+    if (diagnosis.warnings > 0)
+    {
+        diagnosis_badges.push({text: diagnosis.warnings + ' warnings', variant: "warning", icon: "exclamation-triangle" })
+    }
+    if (diagnosis.errors > 0)
+    {
+        diagnosis_badges.push({text: diagnosis.errors + ' issues', variant: "danger", icon: "times" })
+    }
+
+    return {apps_badges, upgrades_badges, diagnosis_badges}
+  })
+
 </script>
 
 <template>
@@ -29,6 +67,10 @@ const menu = [
       >
         <YIcon :iname="item.icon" class="lg ms-1" />
         <h4>{{ $t(item.translation) }}</h4>
+        <YBadgeList v-if="item.routeName == 'app-list'" :badges="apps_badges" />
+        <YBadgeList v-if="item.routeName == 'update'" :badges="upgrades_badges" />
+        <YBadgeList v-if="item.routeName == 'diagnosis'" :badges="diagnosis_badges" />
+
         <YIcon iname="chevron-right" class="lg fs-sm ms-auto" />
       </BListGroupItem>
     </BListGroup>
