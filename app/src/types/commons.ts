525cdead902e170af04a68f66603fba4e99aaf40
Alexandre Aubin <alex.aubin@mailoo.org>
2025-09-01 20:45:21 +0200

    Add YBadgeList component to be injected in other components to display pill badges with specific icon / styles

--- app/src/types/commons.ts
+++ app/src/types/commons.ts
@@ -27,6 +27,11 @@ export type RouteFromTo = Record<'to' | 'from', RouteLocationNormalized>
 export type Translation = string | Record<string, string>
 export type StateVariant = 'success' | 'info' | 'warning' | 'danger'
 export type StateStatus = 'success' | 'info' | 'warning' | 'error'
+export type BadgeInfos = {
+  text: string
+  variant: string
+  icon?: string
+}
 
 // HELPERS
 
